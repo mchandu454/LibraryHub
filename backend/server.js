83e75f23cfc58cee3e97f4c4a957bc38cacb0cd0
Chandra Sekhar Miriyala <miriyalachandrasekharnaidu@gmail.com>
2025-06-27 22:40:05 +0530

    updated port

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 sequelize.authenticate().then(() => {
   console.log("Connected to PostgreSQL ✅");
   app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Server running on port ${PORT}`);
   });
 }).catch(err => {
   console.error("DB connection error ❌", err);
